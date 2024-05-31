7631af4c585823f0633f0816fc40d54d105a365e
Anton Yakovlev <Kolomin58@gmail.com>
2024-05-31 23:19:01 +0300

    new user column

--- backend/src/core/models/user.py
+++ backend/src/core/models/user.py
@@ -1,8 +1,12 @@
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import UniqueConstraint
 
 from .base import Base
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
+    active_user: Mapped[bool] = mapped_column()
+
+    __table_args__ = tuple(UniqueConstraint("username", "active_user"))
