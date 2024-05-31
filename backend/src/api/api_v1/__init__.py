b080d6501342b74d2cdda7efb17b3bc40b6891b5
Anton Yakovlev <Kolomin58@gmail.com>
2024-05-31 23:47:53 +0300

    add api_v1

--- backend/src/api/api_v1/__init__.py
+++ backend/src/api/api_v1/__init__.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from core.config import settings
 from .users import router as users_router
 
-router = APIRouter(pregix=settings.api.v1.prefix)
+router = APIRouter(prefix=settings.api.v1.prefix)
 
 router.include_router(users_router)
--- backend/src/api/api_v1/users.py
+++ backend/src/api/api_v1/users.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.api_v1.crud.users import get_all_users
 from core.config import settings
+from core.models import db_helper
+from core.schemas.user import UserRead
 
 router = APIRouter(prefix=settings.api.v1.users, tags=["Users"])
 
 
-@router.get("/")
-async def get_users():
-    pass
+@router.get("/", response_model=list[UserRead])
+async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
+    users = await get_all_users(session=session)
+    return users
