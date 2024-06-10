a89f2f64745038c2e7b0737027d3f514b5cfdc62
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-10 22:54:58 +0300

    add ID mixin

--- backend/src/core/models/base.py
+++ backend/src/core/models/base.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+
 from sqlalchemy.orm import declared_attr
 from utils.table_name_formatter import camel_case_to_snake_case
 
@@ -11,5 +10,3 @@ class Base(DeclarativeBase):
     @declared_attr
     def __tablename__(cls) -> str:
         return camel_case_to_snake_case(cls.__name__)
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- backend/src/core/models/user.py
+++ backend/src/core/models/user.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy import UniqueConstraint
 
 from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
 
 
-class User(Base):
+class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str] = mapped_column()
     active_user: Mapped[bool] = mapped_column()
