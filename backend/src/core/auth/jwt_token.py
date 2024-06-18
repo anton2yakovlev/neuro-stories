665d2ac8a44d4cf26375e5d0cb1888827624dd51
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-18 22:34:03 +0300

    add default jwt token settings

--- backend/src/core/auth/jwt_token.py
+++ backend/src/core/auth/jwt_token.py
@@ -6,16 +6,17 @@ from core.config import settings
 
 def encode_jwt(
     payload: dict[str, Any],
-    key: str | bytes,
-    algorithm: str | None = "HS256",
+    private_key: str | bytes = settings.auth_jwt.private_key_path.read_text(),
+    algorithm: str | None = settings.auth_jwt.algorithm,
 ):
-    encoded = jwt.encode(payload, key, algorithm=algorithm)
+    encoded = jwt.encode(payload, private_key, algorithm=algorithm)
     return encoded
 
 
 def decode_jwt(
     token: str | bytes,
-    public_key: str | bytes,
-    algorithm: str | None = "HS256",
+    public_key: str | bytes = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str | None = settings.auth_jwt.algorithm,
 ):
     decoded = jwt.decode(token, public_key, algorithms=[algorithm])
+    return decoded
