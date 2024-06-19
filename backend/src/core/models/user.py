dadc1f28007e205484537742a9374ee630e781b8
Anton Yakovlev <Kolomin58@gmail.com>
2024-06-19 21:54:12 +0300

    migration user password type

--- backend/src/core/models/user.py
+++ backend/src/core/models/user.py
@@ -9,7 +9,7 @@ from .mixins.int_id_pk import IntIdPkMixin
 class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
-    hashed_password: Mapped[str] = mapped_column()
+    hashed_password: Mapped[bytes] = mapped_column()
     active: Mapped[bool] = mapped_column(default=True)
 
     __table_args__ = tuple(UniqueConstraint("username", "active_user"))
